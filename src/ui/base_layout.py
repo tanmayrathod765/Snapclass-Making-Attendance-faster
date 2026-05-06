663583aa781c9eba55d886ae8957ed76e3a8a37d
tanmayrathod765 <tanmayrathod765@users.noreply.github.com>
2026-05-06 19:32:25 +0530

    Change header text color to transparent grey

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -62,7 +62,7 @@ def style_base_layout():
                 font-size: 3.5rem !important;
                 line-height:1.1 1important;
                 margin-bottom:0rem !important;
-                color: black !important;
+                color: rgba(70, 70, 70, 0.85) !important;
             }
                 
 
@@ -71,12 +71,12 @@ def style_base_layout():
                 font-size: 2rem !important;
                 line-height:0.9 !important;
                 margin-bottom:0rem !important;
-                color: black !important;
+                color: rgba(70, 70, 70, 0.85) !important;
             }
                 
             h3, h4, p {
                 font-family: 'Outfit', sans-serif;
-                color: black !important;
+                color: rgba(70, 70, 70, 0.85) !important;
             }
                 
 
