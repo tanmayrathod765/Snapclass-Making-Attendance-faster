0f6b6d8bd4d6f799091c98018bbf279a5b0ce57f
tanmayrathod765 <tanmayrathod765@users.noreply.github.com>
2026-05-06 19:29:13 +0530

    Fix header text color from white to black for better readability on light backgrounds

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -62,6 +62,7 @@ def style_base_layout():
                 font-size: 3.5rem !important;
                 line-height:1.1 1important;
                 margin-bottom:0rem !important;
+                color: black !important;
             }
                 
 
@@ -70,10 +71,12 @@ def style_base_layout():
                 font-size: 2rem !important;
                 line-height:0.9 !important;
                 margin-bottom:0rem !important;
+                color: black !important;
             }
                 
             h3, h4, p {
-                font-family: 'Outfit', sans-serif;    
+                font-family: 'Outfit', sans-serif;
+                color: black !important;
             }
                 
 
